c8589b1c04d5776bb04727c3931d0f64a643bb27
Luiz Bitencourt <luizarthur.lp@gmail.com>
2022-12-20 12:06:01 -0300

    Page Header and Sales first modifications

--- src/components/Sales.tsx
+++ src/components/Sales.tsx
@@ -1,16 +1,23 @@
-import { Carousel } from 'react-responsive-carousel'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 export function Sales() {
     return(
         <>
             <h2 className="my-2 text-center text-white font-sans">Promoções do dia</h2>
-            <Carousel showArrows={false} infiniteLoop={true} autoPlay={true} interval={6000}>
-                <div>
-                    <img src="src\assets\promo1.png" alt="" />
-                </div>
-                <div>
-                    <img src="src\assets\promo2.jpg" alt="" />
-                </div>
+            <Carousel controls={false} keyboard={true} interval={5000} pause={'hover'} className="w-full h-[50vh] items-center justify-center" >
+                <Carousel.Item>
+                    <div>
+                        <img src="https://via.placeholder.com/150" alt="" className="w-auto h-[50vh]" />
+                        <p>Teste</p>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div>
+                        <img src="src\assets\promo2.jpg" alt="" className="w-auto h-auto" />
+                    </div>
+                </Carousel.Item>
             </Carousel>
         </>
     )
